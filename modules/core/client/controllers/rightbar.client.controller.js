058aa980be60d4c08a734539a0a223f45dc1d1e9
Trenton D. Adams <trent@trentonadams.ca>
2016-09-09 17:43:02 -0600

    - add rightbar nav

---
+++ modules/core/client/controllers/rightbar.client.controller.js
@@ -0,0 +1,20 @@
+'use strict'
+
+angular.module('core').controller('RightbarController', ['$scope', 'Authentication', 'Menus',
+	function($scope, Authentication, Menus) {
+		$scope.authentication = Authentication;
+		$scope.isCollapsed = false;
+
+		$scope.menu = Menus.getMenu('rightbar');
+        console.log('rightbar: ', $scope.menu);
+
+		$scope.toggleCollapsibleMenu = function() {
+			$scope.isCollapsed = !$scope.isCollapsed;
+		};
+
+		// Collapsing the menu after navigation
+		$scope.$on('$stateChangeSuccess', function() {
+			$scope.isCollapsed = false;
+		});
+	}
+]);
