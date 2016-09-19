3102f5b2afa0366fbb8753362b23d91ac0ec369d
Trenton D. Adams <trent@trentonadams.ca>
2016-09-18 18:04:08 -0600

    - working rightbar - should be working ngmaterials

--- config/assets/default.js
+++ config/assets/default.js
@@ -5,18 +5,30 @@ module.exports = {
     lib: {
       css: [
         'public/lib/bootstrap/dist/css/bootstrap.css',
-        'public/lib/bootstrap/dist/css/bootstrap-theme.css'
+        'public/lib/bootstrap/dist/css/bootstrap-theme.css',
+//        'public/lib/angular-material/layouts/angular-material.min.css'
+        'public/lib/angular-material/angular-material.css',
+        'public/custom/custom.css',
+        'public/custom/offcanvas.css',
+        'public/custom/required-fields.css',
+        'public/custom/theme.css',
+        'public/lib/jquery-ui/themes/base/jquery-ui.css',
       ],
       js: [
         'public/lib/angular/angular.js',
         'public/lib/angular-resource/angular-resource.js',
         'public/lib/angular-animate/angular-animate.js',
+        'public/lib/angular-aria/angular-aria.js',
+        'public/lib/angular-material/angular-material.js',
         'public/lib/angular-messages/angular-messages.js',
         'public/lib/angular-ui-router/release/angular-ui-router.js',
         'public/lib/angular-ui-utils/ui-utils.js',
         'public/lib/angular-bootstrap/ui-bootstrap-tpls.js',
         'public/lib/angular-file-upload/angular-file-upload.js',
-        'public/lib/owasp-password-strength-test/owasp-password-strength-test.js'
+        'public/lib/owasp-password-strength-test/owasp-password-strength-test.js',
+        'public/lib/jquery/dist/jquery.js',
+        'public/custom/offcanvas.js',
+        'public/lib/jquery-ui/jquery-ui.js',
       ],
       tests: ['public/lib/angular-mocks/angular-mocks.js']
     },
