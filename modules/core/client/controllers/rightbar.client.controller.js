721576e028a2a80ad1db3324769732fb17f75623
Trenton D. Adams <trent@trentonadams.ca>
2016-09-18 22:34:59 -0600

    - incorrect code-style, failed lint

--- modules/core/client/controllers/rightbar.client.controller.js
+++ modules/core/client/controllers/rightbar.client.controller.js
@@ -1,20 +1,24 @@
 'use strict';
 
-angular.module('core').controller('RightbarController', ['$scope', 'Authentication', 'Menus',
-	function($scope, Authentication, Menus) {
-		$scope.authentication = Authentication;
-		$scope.isCollapsed = false;
+angular.module('core').controller('RightbarController', [
+  '$scope', 'Authentication', 'Menus',
+  function ($scope, Authentication, Menus)
+  {
+    $scope.authentication = Authentication;
+    $scope.isCollapsed = false;
 
-		$scope.menu = Menus.getMenu('rightbar');
-        console.log('rightbar: ', $scope.menu);
+    $scope.menu = Menus.getMenu('rightbar');
+    console.log('rightbar: ', $scope.menu);
 
-		$scope.toggleCollapsibleMenu = function() {
-			$scope.isCollapsed = !$scope.isCollapsed;
-		};
+    $scope.toggleCollapsibleMenu = function ()
+    {
+      $scope.isCollapsed = !$scope.isCollapsed;
+    };
 
-		// Collapsing the menu after navigation
-		$scope.$on('$stateChangeSuccess', function() {
-			$scope.isCollapsed = false;
-		});
-	}
+    // Collapsing the menu after navigation
+    $scope.$on('$stateChangeSuccess', function ()
+    {
+      $scope.isCollapsed = false;
+    });
+  }
 ]);
